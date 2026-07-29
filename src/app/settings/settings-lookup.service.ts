d5974371e87c17d59745ef0ee4a14fcd313953a6
wdabdu1 <wdabdu@gmail.com>
2026-07-29 14:49:11 +0200

    Implement SettingsLookupService for API interactions

---
+++ src/app/settings/settings-lookup.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_URL } from '../api-config';
+
+export interface LookupEntity {
+  id: number;
+  isActive?: boolean;
+  [key: string]: unknown;
+}
+
+@Injectable({ providedIn: 'root' })
+export class SettingsLookupService {
+  constructor(private http: HttpClient) {}
+
+  getAll<T extends LookupEntity>(resource: string) {
+    return this.http.get<T[]>(`${API_URL}/settings/${resource}`);
+  }
+
+  create<T extends LookupEntity>(resource: string, entity: Partial<T>) {
+    return this.http.post<T>(`${API_URL}/settings/${resource}`, entity);
+  }
+
+  update<T extends LookupEntity>(resource: string, id: number, entity: Partial<T>) {
+    return this.http.put(`${API_URL}/settings/${resource}/${id}`, entity);
+  }
+}
