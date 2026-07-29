64984c3b24523b84b9cccb8b6f7c0048ecd5f525
wdabdu1 <wdabdu@gmail.com>
2026-07-29 14:43:00 +0200

    Update HTTP client configuration with interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // 👈 1. Import this
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './auth/auth.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient() // 👈 2. Add this here
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
