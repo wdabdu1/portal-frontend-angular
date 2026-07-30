1698163d2be0addba5a4abde4eb2b2371294cbc7
wdabdu1 <wdabdu@gmail.com>
2026-07-30 09:07:29 +0200

    Add new route for NewSupplierOrder component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { authGuard } from './auth/auth.guard';
 import { Login } from './auth/login/login';
 import { BusinessUnits } from './settings/business-units/business-units';
+import { NewSupplierOrder } from './purchase-orders/new-supplier-order/new-supplier-order';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'settings/business-units', component: BusinessUnits, canActivate: [authGuard] }
+  { path: 'settings/business-units', component: BusinessUnits, canActivate: [authGuard] },
+  { path: 'orders/new', component: NewSupplierOrder, canActivate: [authGuard] }
 ];
