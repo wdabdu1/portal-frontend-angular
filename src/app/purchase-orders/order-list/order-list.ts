2e10f57a67687b65a17a9072a545adbe8f0f7a63
wdabdu1 <wdabdu@gmail.com>
2026-07-30 10:27:24 +0200

    Create order-list.ts

---
+++ src/app/purchase-orders/order-list/order-list.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
+import { PurchaseOrderSummary, PurchaseOrdersService } from '../purchase-orders.service';
+
+@Component({
+  selector: 'app-order-list',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './order-list.html'
+})
+export class OrderList implements OnInit {
+  private cdr = inject(ChangeDetectorRef);
+
+  orders: PurchaseOrderSummary[] = [];
+  loading = true;
+  error = '';
+  confirmingId: number | null = null;
+
+  constructor(private ordersService: PurchaseOrdersService, public auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load(): void {
+    this.loading = true;
+    this.ordersService.getAll().subscribe({
+      next: (orders) => {
+        this.orders = orders;
+        this.loading = false;
+        this.cdr.markForCheck();
+      },
+      error: () => {
+        this.error = 'Could not load purchase orders.';
+        this.loading = false;
+        this.cdr.markForCheck();
+      }
+    });
+  }
+
+  confirm(id: number): void {
+    this.confirmingId = id;
+    this.ordersService.confirm(id).subscribe({
+      next: () => {
+        this.confirmingId = null;
+        this.load();
+      },
+      error: () => {
+        this.confirmingId = null;
+        this.error = 'Could not confirm this order.';
+        this.cdr.markForCheck();
+      }
+    });
+  }
+}
