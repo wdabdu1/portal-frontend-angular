0a714bd86a53b7a15534b8c53e5e70c4f543cec6
wdabdu1 <wdabdu@gmail.com>
2026-07-30 10:23:06 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { authGuard } from './auth/auth.guard';
 import { Login } from './auth/login/login';
 import { BusinessUnits } from './settings/business-units/business-units';
 import { NewSupplierOrder } from './purchase-orders/new-supplier-order/new-supplier-order';
+import { OrderList } from './purchase-orders/order-list/order-list';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'orders', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'settings/business-units', component: BusinessUnits, canActivate: [authGuard] },
+  { path: 'orders', component: OrderList, canActivate: [authGuard] },
   { path: 'orders/new', component: NewSupplierOrder, canActivate: [authGuard] }
 ];
