6c806ddef2a6ffe7dc0976b36f567a98267bc3ee
wdabdu1 <wdabdu@gmail.com>
2026-07-30 12:50:16 +0200

    Add route for updating shipments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { NewSupplierOrder } from './purchase-orders/new-supplier-order/new-suppl
 import { OrderList } from './purchase-orders/order-list/order-list';
 import { NewShipment } from './shipments/new-shipment/new-shipment';
 import { ShipmentList } from './shipments/shipment-list/shipment-list';
+import { UpdateShipment } from './shipments/update-shipment/update-shipment';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'orders', pathMatch: 'full' },
@@ -15,6 +16,7 @@ export const routes: Routes = [
 
   { path: 'settings', component: SettingsMenu, canActivate: [authGuard] },
   { path: 'settings/business-units', component: BusinessUnits, canActivate: [authGuard] },
+  { path: 'shipments/:id', component: UpdateShipment, canActivate: [authGuard] },
 
   {
     path: 'settings/business-partners',
