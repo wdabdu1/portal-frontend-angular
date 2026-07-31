f069b891983054dd751d1cb25d7d78514c4994cf
wdabdu1 <wdabdu@gmail.com>
2026-07-31 20:25:02 +0200

    Update clearance.service.ts

--- src/app/clearance/clearance.service.ts
+++ src/app/clearance/clearance.service.ts
@@ -16,6 +16,34 @@ export interface ClearanceShipmentSummary {
   routeStatus: string;
 }
 
+export interface ClearanceRoute1Details {
+  moveRequestDate: string | null; billAmountSdg: number | null; billSettlementDate: string | null;
+  ssmoFileRequestDate: string | null; ssmoInspectionAmountSdg: number | null; ssmoFeesSettlementDate: string | null;
+  custExamStartDate: string | null; custExamCompletedDate: string | null;
+  customsLabRequired: boolean; customsLabFeesSdg: number | null; labFeesPaymentDate: string | null; labResultIssuanceDate: string | null;
+  ssmoExamStartDate: string | null; ssmoCertIssuanceDate: string | null;
+  custEvaluationDate: string | null; customsDutySdg: number | null; customsSettlementDate: string | null; releaseExitPassDate: string | null;
+  spcBillRequestDate: string | null; spcBillValueSdg: number | null; spcBillSettlementDate: string | null;
+  truckPortEntryPermitDate: string | null; containersReturnedDate: string | null; clearanceActualCompletedDate: string | null;
+}
+
+export interface ClearanceRoute2Details {
+  depositRequestDate: string | null; requestApprovalDate: string | null;
+  inspectionDate: string | null;
+  spcBillRequestDate: string | null; spcBillValueSdg: number | null; spcBillSettlementDate: string | null; policeSecurityAppointedDate: string | null;
+  truckPortEntryPermitDate: string | null; containersReceivedAtFzDate: string | null; containersReturnedDate: string | null; clearanceActualCompletedDate: string | null;
+}
+
+export interface ClearanceRoute3Details {
+  certificateEntryDate: string | null; scudaDeclarationNo: string | null;
+  ssmoFileRequestDate: string | null; ssmoInspectionAmountSdg: number | null; ssmoFeesSettlementDate: string | null;
+  custExamStartDate: string | null; custExamCompletedDate: string | null;
+  customsLabRequired: boolean; customsLabFeesSdg: number | null; labFeesPaymentDate: string | null; labResultIssuanceDate: string | null;
+  ssmoExamStartDate: string | null; ssmoCertIssuanceDate: string | null;
+  custEvaluationDate: string | null; customsDutySdg: number | null; customsSettlementDate: string | null; releaseExitPassDate: string | null;
+  truckPortEntryPermitDate: string | null; clearanceActualCompletedDate: string | null;
+}
+
 export interface ClearanceDetail {
   shipmentId: number;
   blAwbNo: string;
@@ -49,4 +77,26 @@ export class ClearanceService {
   setRoute(shipmentId: number, route: number) {
     return this.http.put(`${API_URL}/clearance/${shipmentId}/route`, { route });
   }
+
+  getRoute1(shipmentId: number) {
+    return this.http.get<ClearanceRoute1Details | null>(`${API_URL}/clearance/${shipmentId}/route1`);
+  }
+  saveRoute1(shipmentId: number, req: Partial<ClearanceRoute1Details>) {
+    return this.http.put(`${API_URL}/clearance/${shipmentId}/route1`, req);
+  }
+
+  getRoute2(shipmentId: number) {
+    return this.http.get<ClearanceRoute2Details | null>(`${API_URL}/clearance/${shipmentId}/route2`);
+  }
+  saveRoute2(shipmentId: number, req: Partial<ClearanceRoute2Details>) {
+    return this.http.put(`${API_URL}/clearance/${shipmentId}/route2`, req);
+  }
+
+  getRoute3(shipmentId: number) {
+    return this.http.get<ClearanceRoute3Details | null>(`${API_URL}/clearance/${shipmentId}/route3`);
+  }
+  saveRoute3(shipmentId: number, req: Partial<ClearanceRoute3Details>) {
+    return this.http.put(`${API_URL}/clearance/${shipmentId}/route3`, req);
+  }
+}
 }
