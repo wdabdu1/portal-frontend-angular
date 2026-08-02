f58ce566132106fb661a3f7283adb8732ce0669e
wdabdu1 <wdabdu@gmail.com>
2026-08-02 22:45:19 +0200

    Add OrderDetailsService with purchase order details

---
+++ src/app/purchase-orders/order-details/order-details.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_URL } from '../../api-config';
+
+export interface PoLineItemDetail {
+  productCategory: string;
+  productType: string;
+  modelProduct: string;
+  qty: number;
+  unitOfMeasure: string;
+  unitPrice: number | null;
+  currency: string | null;
+  totalValue: number | null;
+}
+
+export interface PoOffshorePartnerDetail {
+  sequenceOrder: number;
+  name: string;
+  isLast: boolean;
+}
+
+export interface PurchaseOrderDetail {
+  id: number;
+  poNumber: string;
+  status: string;
+  createdAt: string;
+  businessUnit: string;
+  division: string | null;
+  supplier: string | null;
+  brandManufacturer: string;
+  consignee: string;
+  incoterm: string;
+  paymentTerm: string;
+  approvalType: string;
+  totalOrderValueUsd: number | null;
+  lineItems: PoLineItemDetail[];
+  offshorePartners: PoOffshorePartnerDetail[];
+}
+
+@Injectable({ providedIn: 'root' })
+export class OrderDetailsService {
+  constructor(private http: HttpClient) {}
+
+  get(id: number) {
+    return this.http.get<PurchaseOrderDetail>(`${API_URL}/orders/${id}/details`);
+  }
+}
