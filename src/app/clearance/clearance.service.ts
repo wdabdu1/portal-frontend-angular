1741459a063ceb9ffffaaff4b2d63dddbecbcb7e
wdabdu1 <wdabdu@gmail.com>
2026-08-03 07:35:49 +0200

    Update clearance.service.ts

--- src/app/clearance/clearance.service.ts
+++ src/app/clearance/clearance.service.ts
@@ -130,7 +130,6 @@ export interface DemurrageStorageResult {
   totalStorageDemurrageSdg: number;
   warnings: string[];
 }
-}
 
 @Injectable({ providedIn: 'root' })
 export class ClearanceService {
