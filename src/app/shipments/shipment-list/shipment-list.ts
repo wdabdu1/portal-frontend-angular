4cc967dbd0d2f3264912fda064e5ee0cbd312b49
wdabdu1 <wdabdu@gmail.com>
2026-08-04 10:53:05 +0200

    Update shipment-list.ts

--- src/app/shipments/shipment-list/shipment-list.ts
+++ src/app/shipments/shipment-list/shipment-list.ts
@@ -3,6 +3,8 @@ import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { ShipmentSummary, ShipmentsService } from '../shipments.service';
 
+type SortColumn = keyof ShipmentSummary;
+
 @Component({
   selector: 'app-shipment-list',
   imports: [CommonModule, RouterLink],
@@ -11,9 +13,12 @@ import { ShipmentSummary, ShipmentsService } from '../shipments.service';
 export class ShipmentList implements OnInit {
   private cdr = inject(ChangeDetectorRef);
 
-  shipments: ShipmentSummary[] = [];
+  allShipments: ShipmentSummary[] = [];
   loading = true;
   error = '';
+
+  sortColumn: SortColumn = 'blAwbNo';
+  sortAsc = true;
 
   constructor(private shipmentsService: ShipmentsService, private router: Router) {}
 
@@ -24,7 +29,7 @@ export class ShipmentList implements OnInit {
   ngOnInit(): void {
     this.shipmentsService.getAll().subscribe({
       next: (shipments) => {
-        this.shipments = shipments;
+        this.allShipments = shipments;
         this.loading = false;
         this.cdr.markForCheck();
       },
@@ -34,5 +39,27 @@ export class ShipmentList implements OnInit {
         this.cdr.markForCheck();
       }
     });
+  }
+
+  get shipments(): ShipmentSummary[] {
+    const dir = this.sortAsc ? 1 : -1;
+    return [...this.allShipments].sort((a, b) => {
+      const av = a[this.sortColumn];
+      const bv = b[this.sortColumn];
+      if (av === null || av === undefined) return 1;
+      if (bv === null || bv === undefined) return -1;
+      if (av < bv) return -1 * dir;
+      if (av > bv) return 1 * dir;
+      return 0;
+    });
+  }
+
+  sortBy(column: SortColumn): void {
+    if (this.sortColumn === column) {
+      this.sortAsc = !this.sortAsc;
+    } else {
+      this.sortColumn = column;
+      this.sortAsc = true;
+    }
   }
 }
