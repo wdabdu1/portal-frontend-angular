1a6820bf9b51f5cf916d199690ff809abe31f6a8
wdabdu1 <wdabdu@gmail.com>
2026-08-05 15:01:03 +0200

    Update shipment-list.ts

--- src/app/shipments/shipment-list/shipment-list.ts
+++ src/app/shipments/shipment-list/shipment-list.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { TablePreferencesService } from '../../table-preferences/table-preferences.service';
 import { ShipmentSummary, ShipmentsService } from '../shipments.service';
 
 type SortColumn = keyof ShipmentSummary;
 
 @Component({
   selector: 'app-shipment-list',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './shipment-list.html'
 })
 export class ShipmentList implements OnInit {
@@ -20,13 +22,29 @@ export class ShipmentList implements OnInit {
   sortColumn: SortColumn = 'blAwbNo';
   sortAsc = true;
 
-  constructor(private shipmentsService: ShipmentsService, private router: Router) {}
+  filterBusinessUnit = '';
+  filterStatus = '';
+
+  constructor(private shipmentsService: ShipmentsService, private router: Router, private tablePrefs: TablePreferencesService) {}
 
   viewDetails(id: number): void {
     this.router.navigate(['/shipments', id]);
   }
 
   ngOnInit(): void {
+    this.tablePrefs.get('shipments').subscribe({
+      next: (pref) => {
+        if (pref) {
+          this.sortColumn = pref.sortColumn as SortColumn;
+          this.sortAsc = pref.sortAsc;
+        }
+        this.load();
+      },
+      error: () => this.load()
+    });
+  }
+
+  load(): void {
     this.shipmentsService.getAll().subscribe({
       next: (shipments) => {
         this.allShipments = shipments;
@@ -41,9 +59,21 @@ export class ShipmentList implements OnInit {
     });
   }
 
+  get businessUnitOptions(): string[] {
+    return [...new Set(this.allShipments.map((s) => s.businessUnit))].sort();
+  }
+
+  get statusOptions(): string[] {
+    return [...new Set(this.allShipments.map((s) => s.status))].sort();
+  }
+
   get shipments(): ShipmentSummary[] {
+    let filtered = this.allShipments;
+    if (this.filterBusinessUnit) filtered = filtered.filter((s) => s.businessUnit === this.filterBusinessUnit);
+    if (this.filterStatus) filtered = filtered.filter((s) => s.status === this.filterStatus);
+
     const dir = this.sortAsc ? 1 : -1;
-    return [...this.allShipments].sort((a, b) => {
+    return [...filtered].sort((a, b) => {
       const av = a[this.sortColumn];
       const bv = b[this.sortColumn];
       if (av === null || av === undefined) return 1;
@@ -61,5 +91,6 @@ export class ShipmentList implements OnInit {
       this.sortColumn = column;
       this.sortAsc = true;
     }
+    this.tablePrefs.save('shipments', this.sortColumn, this.sortAsc).subscribe();
   }
 }
