d12ef6c88357571d01205046b23e13e402b6cf35
wdabdu1 <wdabdu@gmail.com>
2026-08-05 14:58:01 +0200

    Create table-preferences.service.ts

---
+++ src/app/table-preferences/table-preferences.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_URL } from '../api-config';
+
+export interface TableSortPreference {
+  sortColumn: string;
+  sortAsc: boolean;
+}
+
+@Injectable({ providedIn: 'root' })
+export class TablePreferencesService {
+  constructor(private http: HttpClient) {}
+
+  get(tableKey: string) {
+    return this.http.get<TableSortPreference | null>(`${API_URL}/table-preferences/${tableKey}`);
+  }
+
+  save(tableKey: string, sortColumn: string, sortAsc: boolean) {
+    return this.http.put(`${API_URL}/table-preferences/${tableKey}`, { sortColumn, sortAsc });
+  }
+}
