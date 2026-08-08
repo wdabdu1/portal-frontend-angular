5c9b1b9ef440fca43856b58a9b3f9135d3e9d92c
wdabdu1 <wdabdu@gmail.com>
2026-08-08 09:12:18 +0200

    Create transfer-pricing.service.ts

---
+++ src/app/transfer-pricing/transfer-pricing.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_URL } from '../api-config';
+
+export interface TpStage {
+  purchaseOrderOffshorePartnerId: number;
+  companyName: string;
+  sequenceOrder: number;
+  isLast: boolean;
+  markupPercent: number | null;
+  currencyId: number | null;
+  currencyCode: string | null;
+  total: number | null;
+  totalUsd: number | null;
+}
+
+export interface TpLineItem {
+  shipmentLineItemId: number;
+  businessUnit: string;
+  category: string;
+  modelProduct: string;
+  blAwbNo: string;
+  supplierTotal: number;
+  supplierCurrencyCode: string;
+  supplierCnfUsd: number;
+  stages: TpStage[];
+}
+
+@Injectable({ providedIn: 'root' })
+export class TransferPricingService {
+  constructor(private http: HttpClient) {}
+
+  getShipment(shipmentId: number) {
+    return this.http.get<TpLineItem[]>(`${API_URL}/transfer-pricing/${shipmentId}`);
+  }
+
+  saveLineItem(shipmentLineItemId: number, stages: { purchaseOrderOffshorePartnerId: number; currencyId: number; markupPercent: number | null }[]) {
+    return this.http.put(`${API_URL}/transfer-pricing/line-item/${shipmentLineItemId}`, { stages });
+  }
+}
