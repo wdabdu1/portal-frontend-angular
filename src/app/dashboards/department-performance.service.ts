26b3292e11bc8f9e13f4d7c512eae4a123b28aea
wdabdu1 <wdabdu@gmail.com>
2026-08-12 23:00:53 +0200

    Create department-performance.service.ts

---
+++ src/app/dashboards/department-performance.service.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_URL } from '../api-config';
+
+export interface DemurrageHitDetail {
+  blAwbNo: string;
+  businessUnit: string;
+  demurrageStorageUsd: number;
+  shipmentValueUsd: number;
+  magnitudePercent: number;
+}
+
+export interface DepartmentPerformanceResponse {
+  orderCount: number;
+  orderValueUsd: number;
+  executionPercent: number;
+  draftCount: number;
+  inTransitCount: number;
+  underClearanceCount: number;
+  deliveredCount: number;
+  draftValueUsd: number;
+  inTransitValueUsd: number;
+  underClearanceValueUsd: number;
+  deliveredValueUsd: number;
+  depositCount: number;
+  withdrawalCount: number;
+  shipmentsHitCount: number;
+  totalDemurrageStorageUsd: number;
+  totalShipmentValueUsd: number;
+  overallMagnitudePercent: number;
+  hitDetails: DemurrageHitDetail[];
+}
+
+export interface DepartmentPerformanceFilters {
+  etaFrom?: string;
+  etaTo?: string;
+  businessUnitId?: number;
+  consigneeId?: number;
+  shippingLineId?: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class DepartmentPerformanceService {
+  constructor(private http: HttpClient) {}
+
+  get(filters: DepartmentPerformanceFilters) {
+    let params = '';
+    const parts: string[] = [];
+    if (filters.etaFrom) parts.push(`etaFrom=${filters.etaFrom}`);
+    if (filters.etaTo) parts.push(`etaTo=${filters.etaTo}`);
+    if (filters.businessUnitId) parts.push(`businessUnitId=${filters.businessUnitId}`);
+    if (filters.consigneeId) parts.push(`consigneeId=${filters.consigneeId}`);
+    if (filters.shippingLineId) parts.push(`shippingLineId=${filters.shippingLineId}`);
+    if (parts.length > 0) params = '?' + parts.join('&');
+
+    return this.http.get<DepartmentPerformanceResponse>(`${API_URL}/dashboards/department-performance${params}`);
+  }
+}
