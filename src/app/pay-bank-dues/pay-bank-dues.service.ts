4d93dedc4bb7b4506dd85d10322abe04ebaa7ebd
wdabdu1 <wdabdu@gmail.com>
2026-08-21 15:18:55 +0200

    Create pay-bank-dues.service.ts

---
+++ src/app/pay-bank-dues/pay-bank-dues.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_URL } from '../api-config';
+
+export interface PayableDueRow {
+  shipmentId: number;
+  blAwbNo: string;
+  category: string;
+  invoiceNo: string | null;
+  collectionRefNo: string | null;
+  valueAed: number;
+  paidAed: number;
+  remainingAed: number;
+}
+
+export interface SenderBankOption {
+  id: number;
+  name: string;
+}
+
+export interface ConfirmLine {
+  shipmentId: number;
+  paymentAmountAed: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class PayBankDuesService {
+  constructor(private http: HttpClient) {}
+
+  getSenderBanks(receiverBankId: number) {
+    return this.http.get<SenderBankOption[]>(`${API_URL}/pay-bank-dues/sender-banks`, { params: { receiverBankId } });
+  }
+
+  getDues(receiverBankId: number, senderBankId: number) {
+    return this.http.get<PayableDueRow[]>(`${API_URL}/pay-bank-dues/dues`, { params: { receiverBankId, senderBankId } });
+  }
+
+  confirm(receiverBankId: number, senderBankId: number, accountId: number, lines: ConfirmLine[]) {
+    return this.http.post(`${API_URL}/pay-bank-dues/confirm`,
+      { receiverBankId, senderBankId, accountId, lines },
+      { responseType: 'blob', observe: 'response' });
+  }
+}
