f0fb1dd465e4d9baae5d80d7f0b43efd4644b0bb
wdabdu1 <wdabdu@gmail.com>
2026-08-22 16:25:06 +0200

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -72,7 +72,7 @@ const MENU_GROUPS: MenuGroup[] = [
       { label: 'Transfer Pricing', route: '/transfer-pricing', canAccess: (a) => a.hasRole('CorpFinance') || a.hasRole('Manager') || a.hasRole('SuperUser') },
       { label: 'Supplier Dues', route: '/supplier-dues', canAccess: (a) => a.canSeeSupplierDues() },
       { label: 'Bank Dues', route: '/bank-dues', canAccess: (a) => a.canSeeBankDues() },
-      { label: 'Price History', route: '/price-history', canAccess: (a) => a.canSeeBankDues() },
+      { label: 'CP History', route: '/price-history', canAccess: (a) => a.canSeeBankDues() },
       { label: 'Pay Bank Dues', route: '/pay-bank-dues', canAccess: (a) => a.canSeePayBankDues() }
     ]
   }
